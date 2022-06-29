db1e41bfa292a78737a0360e5d9029894980b17d
pranavMeshram <pranav.meshram@globallogic.com>
2022-06-29 08:09:44 +0530

    basic setup for movies app

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Text, View } from 'react-native';
 import Colors from './Colors';
+import { getPopularMovies } from './services/services';
+
+
 
 const App = () => {
+  const [movies, setMovies] = useState('');
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getPopularMovies().then(movies => {
+      setMovies(movies[0]);
+    }).catch(err => {
+      setError(err);
+    });
+  }, [])
+
+
   return (
-    < Colors />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Movie Name: {movies.original_title}</Text>
+      <Text>Release Date: {movies.release_date}</Text>
+      {error && <Text style={{ color: 'red' }}>Error in the server</Text>}
+    </View>
   );
 };
 
---
+++ services/servicesUrl.js
@@ -0,0 +1,10 @@
+
+//eg: https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+
+export const moviesUrl = {
+    BASE_URL: "https://api.themoviedb.org/3/",
+    POPULAR_MOVIES_URL: "movie/popular/",
+    API_KEY: "api_key=fefa9c369cd17b76463335d240ac21ee",
+
+}
+
