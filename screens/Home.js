4da66c33695cbd47b7749c1987ee8fa5d6a9b7b5
pranavMeshram <pranav.meshram@globallogic.com>
2022-06-30 08:30:54 +0530

    adding more movie types

--- screens/Home.js
+++ screens/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Dimensions, FlatList, ScrollView } from 'react-native';
 import { SliderBox } from "react-native-image-slider-box";
 import List from '../components/List';
-import { getPopularMovies, getUpcomingMovies } from '../services/services';
+import { getPopularMovies, getUpcomingMovies, getPopularTv, getFamilyMovies, getDocumentaryMovies } from '../services/services';
 
 const dimensions = Dimensions.get('screen');
 
@@ -10,6 +10,9 @@ const Home = () => {
 
     const [moviesImages, setMoviesImages] = useState('');
     const [popularMovies, setPopularMovies] = useState('');
+    const [popularTv, setPopularTv] = useState('');
+    const [familyMovies, setFamilyMovies] = useState('');
+    const [documentaryMovies, setDocumentaryMovies] = useState('');
     const [error, setError] = useState(false);
 
     useEffect(() => {
@@ -26,6 +29,25 @@ const Home = () => {
         }).catch(err => {
             setError(err);
         });
+
+        getPopularTv().then(movies => {
+            setPopularTv(movies)
+        }).catch(err => {
+            setError(err);
+        });
+
+        getFamilyMovies().then(movies => {
+            setFamilyMovies(movies)
+        }).catch(err => {
+            setError(err);
+        });
+
+        getDocumentaryMovies().then(movies => {
+            setDocumentaryMovies(movies)
+        }).catch(err => {
+            setError(err);
+        });
+
     }, [])
 
 
@@ -46,6 +68,18 @@ const Home = () => {
 
                 <View style={styles.carousel}>
                     <List title="Popular Movies" content={popularMovies} />
+                </View>
+
+                <View style={styles.carousel}>
+                    <List title="Popular Tv" content={popularTv} />
+                </View>
+
+                <View style={styles.carousel}>
+                    <List title="Family Movies" content={familyMovies} />
+                </View>
+
+                <View style={styles.carousel}>
+                    <List title="Documentary Movies" content={documentaryMovies} />
                 </View>
 
             </ScrollView>
--- services/services.js
+++ services/services.js
@@ -6,13 +6,6 @@ const BASE_UR = "https://api.themoviedb.org/3";
 const API_KEY = "api_key=fefa9c369cd17b76463335d240ac21ee"
 
 
-// Get Popular Movies
-export const getPopularMovies = async () => {
-    const resp = await axios.get(`${BASE_UR}/movie/popular/?${API_KEY}`);
-    return resp.data.results;
-};
-
-
 // Get Upcoming Movies
 export const getUpcomingMovies = async () => {
     const resp = await axios.get(`${BASE_UR}/movie/upcoming/?${API_KEY}`);
@@ -20,8 +13,29 @@ export const getUpcomingMovies = async () => {
 };
 
 
+// Get Popular Movies
+export const getPopularMovies = async () => {
+    const resp = await axios.get(`${BASE_UR}/movie/popular/?${API_KEY}`);
+    return resp.data.results;
+};
+
+
 // Get Popular TV
 export const getPopularTv = async () => {
     const resp = await axios.get(`${BASE_UR}/tv/popular/?${API_KEY}`);
+    return resp.data.results;
+};
+
+
+// Get Family Movies
+export const getFamilyMovies = async () => {
+    const resp = await axios.get(`${BASE_UR}/discover/movie/?${API_KEY}&with_genres=10751`);
+    return resp.data.results;
+};
+
+
+// Get Documentery Movies
+export const getDocumentaryMovies = async () => {
+    const resp = await axios.get(`${BASE_UR}/discover/movie/?${API_KEY}&with_genres=99`);
     return resp.data.results;
 };
